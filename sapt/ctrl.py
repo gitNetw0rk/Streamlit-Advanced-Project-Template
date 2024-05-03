9f738d29b497e3ecb19a4e938501b1e8372fe53a
gitNetw0rk <stancik.network@gmail.com>
2024-05-03 14:45:29 +0200

    updates

--- sapt/ctrl.py
+++ sapt/ctrl.py
@@ -8,3 +8,12 @@ def show_menu():
     st.sidebar.page_link(page="pages/demo_pdis.py", label="Last demo")
     st.sidebar.divider()
     st.sidebar.page_link(page="pages/tutorials.py", label="Tutorials")
+    st.sidebar.markdown(
+        """
+        - [Run Streamlit locally](#run-streamlit-locally)
+        - [Desktop Dev Kit](#101-desktop-developer-kit)
+        - [About](#streamlit-advanced-project-template)
+        """,
+        unsafe_allow_html=True,
+    )
+    # st.sidebar.page_link(page="pages/tutorials.py", label="[Tutorials]#install-streamlit")
